733565329f8ef3996160631ff5ce6d622f53e826
Its-Jakey <102316437+Its-Jakey@users.noreply.github.com>
2022-03-26 18:31:58 -0800

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-constructor() ERC20("NAME", "SYMBOL"){
+contract Token is ERC20{
+    constructor() ERC20("NAME", "SYMBOL"){
         _mint(msg.sender,10000000*10**18);
+    }
 }
